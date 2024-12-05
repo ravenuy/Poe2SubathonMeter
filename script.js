bff595aa9523d4400f6780ff6b2e1c306621689f
ravenuy <santirack21@gmail.com>
2024-12-05 17:23:38 -0300

    Added flags and fixed stuff

--- script.js
+++ script.js
@@ -6,13 +6,15 @@ const gradients = [
   "linear-gradient(135deg, #A330C9, #7E5DBB)", // DH
   "linear-gradient(135deg, #FF7C0A, #FF9A36)", // DUDU
   "linear-gradient(135deg, #1E4D2B, #47B24F)", // Huntard (Hehe)
-  "linear-gradient(135deg, #69CCF0, #2C88D1)", // mage 
+  "linear-gradient(135deg, #00FF96, #00CC66)", // monk
   "linear-gradient(135deg, #F58CBA, #FF8DC7)", // paly
   "linear-gradient(135deg, #FFFFFF, #F4F4F4)", // priestge
   "linear-gradient(135deg, #FFF468, #D8B92A)", // rogue
   "linear-gradient(135deg, #0070DD, #33A8FF)", // shammy
   "linear-gradient(135deg, #8788EE, #5A55B3)", // lock
   "linear-gradient(135deg, #C79C6E, #B68C5B)"  // WARRIOR(GIGACHAD)
+
+  
 ];
 
 
@@ -27,70 +29,72 @@ function shuffleArray(array) {
 // do the stuff xdd
 async function updateRanking() {
   try {
-    
     const response = await fetch(API_URL);
     const data = await response.json();
 
-    
     const totalSubs = data.reduce((acc, item) => acc + item.subsPerHour, 0);
 
-    
     const rankingDiv = document.getElementById("ranking");
-
-    
     rankingDiv.innerHTML = "";
 
-    
     const header = document.createElement("div");
     header.classList.add("header");
     header.innerHTML = `
-      <span class="rank">RANK</span>
       <span class="name">NAME</span>
-      <span class="subs">SUBS PER HOUR</span>
+      <span class="subs">SUBS</span>
     `;
     rankingDiv.appendChild(header);
 
-    
     shuffleArray(gradients);
 
-    
     data.forEach((item, index) => {
       const entryDiv = document.createElement("div");
       entryDiv.classList.add("entry");
 
-      
       const percentage = (item.subsPerHour / totalSubs) * 100;
 
-      
       const overlay = document.createElement("div");
       overlay.classList.add("overlay");
-      overlay.style.width = `${percentage}%`; 
+      overlay.style.width = `${percentage}%`;
 
-      
-      if (item.name === "CuteDog_") {        
-        overlay.style.background = "linear-gradient(135deg, #00FF96, #00CC66)";
+      if (item.name === "CuteDog_") {
+        overlay.style.background = "linear-gradient(135deg, #69CCF0, #2C88D1)"; //blue color requested by cd peepoCute
       } else {
-        
-        const randomGradient = gradients[index % gradients.length]; 
+        const randomGradient = gradients[index % gradients.length];
         overlay.style.background = randomGradient;
       }
 
-      
       entryDiv.appendChild(overlay);
 
-      
       if (item.name === "CuteDog_") {
-        entryDiv.classList.add("glass-effect"); 
+        entryDiv.classList.add("glass-effect");
       }
 
       
+      const flagImg = document.createElement("img");
+      if (item.country === "NA") {
+        flagImg.src = "https://flagcdn.com/w320/us.png";  
+        flagImg.alt = "NA Flag";
+        flagImg.classList.add("flag");
+      } else if (item.country === "EU") {
+        flagImg.src = "https://flagcdn.com/w320/eu.png";  
+        flagImg.alt = "EU Flag";
+        flagImg.classList.add("flag");
+      }else if (item.country === "NZ") {
+        flagImg.src = "https://flagcdn.com/w320/nz.png";  
+        flagImg.alt = "NZ Flag";
+        flagImg.classList.add("flag");
+      }
+
       entryDiv.innerHTML += `
         <span class="rank">${index + 1}</span>
+      `;      
+      entryDiv.appendChild(flagImg);
+      entryDiv.innerHTML += `
         <span class="name">${item.name}</span>
         <span class="subs">${item.subsPerHour}</span>
       `;
 
-      
       rankingDiv.appendChild(entryDiv);
     });
   } catch (error) {
